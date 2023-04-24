e19792927356c322f3ca0dbe554a41c564ec567e
hendre <kimfatt.chin@evdg.cc>
2023-04-25 00:37:38 +0800

    Initial

---
+++ src/test/java/com/hendre/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendre.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
